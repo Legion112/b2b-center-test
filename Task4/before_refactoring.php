89c811c96f7e7a94f3d8f7de71dd3707d856a871
Max Ageev <ageev.maks2014@gmail.com>
2019-12-17 00:15:45 +0300

    messaged

--- Task4/before_refactoring.php
+++ Task4/before_refactoring.php
@@ -1,12 +1,15 @@
 <?php
-
+// better to accept array of id instead of string
+// as well better to have a class instead of function
+// to have ability inject Database connection for testability
 function load_users_data($user_ids)
 {
     $user_ids = explode(',', $user_ids);
     foreach ($user_ids as $user_id) {
-
-
+        // hardcoded configuration should be located in separate file
         $db = mysqli_connect('127.0.0.1', 'root', 'example', 'test');
+        // SQL Injection better to use PDO with prepare statement
+        // WE should specify only data what we need (id, name)
         $sql = mysqli_query($db, "SELECT * FROM users WHERE id=$user_id");
         while ($obj = $sql->fetch_object()) {
             $data[$user_id] = $obj->name;
