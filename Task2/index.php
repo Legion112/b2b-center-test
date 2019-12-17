6684d948edcadd360cc0f695b1534fc24a23a42d
Max Ageev <ageev.maks2014@gmail.com>
2019-12-17 12:51:17 +0300

    url parser

---
+++ Task2/index.php
@@ -0,0 +1,16 @@
+<?php
+
+function url(string $url):string {
+    $parts = parse_url($url);
+    parse_str($parts['query'], $queryParams);
+    $queryParams = array_map('intval', $queryParams);
+    $queryParams = array_filter($queryParams, function ($value) {
+        return $value !== 3;
+    });
+    asort($queryParams);
+    $queryParams['url'] = $parts['path'];
+    return $parts['scheme'] . '://' . $parts['host'] . '?'. http_build_query($queryParams);
+
+}
+
+echo url('https://www.somehost.com/test/index.html?param1=4&param2=3&param3=2&param4=1&param5=3');
